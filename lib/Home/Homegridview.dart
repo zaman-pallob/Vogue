b6179c6d6d3a5b8e9c0f997334b3b73b8c0b6092
Atikur Zaman Pallob <atikurzamanpallob@gmail.com>
2020-07-03 06:57:25 +0600

    product image adding

--- lib/Home/Homegridview.dart
+++ lib/Home/Homegridview.dart
@@ -6,7 +6,38 @@ class Homegridview extends StatefulWidget {
 }
 
 class _HomegridviewState extends State<Homegridview> {
-  List item = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  List item = [
+    {
+      'name': 'Blazer-101',
+      'price': '800\$',
+      'discount': '10 %',
+      'dp': 'products/p1.jpg'
+    },
+    {
+      'name': 'Casual Shirt',
+      'price': '300\$',
+      'discount': '4 %',
+      'dp': 'products/p2.jpeg'
+    },
+    {
+      'name': 'Blue Hill',
+      'price': '200\$',
+      'discount': '0 %',
+      'dp': 'products/p3.jpg'
+    },
+    {
+      'name': 'Blazers-23',
+      'price': '700\$',
+      'discount': '15 %',
+      'dp': 'products/p4.jpg'
+    },
+    {
+      'name': 'Blazer-111',
+      'price': '800\$',
+      'discount': '6 %',
+      'dp': 'products/p5.png'
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
@@ -15,27 +46,42 @@ class _HomegridviewState extends State<Homegridview> {
         gridDelegate:
             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (context, index) {
-          return singleBlock(index, item[index]);
+          return singleBlock(index, item);
         });
   }
 
-  Widget singleBlock(int index, String itemname) {
+  Widget singleBlock(int index, List item) {
     return InkWell(
-      onTap: () {
-        print(itemname);
-      },
+      onTap: () {},
       child: GridTile(
-        footer: ListTile(
-          leading: Text("name"),
-          title: Text("price"),
-          subtitle: Text("discount"),
+        header: ListTile(
+          leading: Text(
+            item[index]['name'],
+            style: TextStyle(
+              color: Colors.redAccent,
+            ),
+          ),
+          title: Text(
+            "Price:" + item[index]['price'],
+            style: TextStyle(
+              fontSize: 13,
+              color: Colors.redAccent,
+            ),
+          ),
+          subtitle: Text(
+            "Dis: " + item[index]['discount'],
+            style: TextStyle(
+              fontSize: 13,
+              color: Colors.redAccent,
+            ),
+          ),
         ),
         child: Container(
           padding: EdgeInsets.all(2),
           child: Card(
             child: Image(
-              image: AssetImage('images/f2.jpg'),
-              fit: BoxFit.cover,
+              image: AssetImage(item[index]['dp']),
+              fit: BoxFit.fill,
             ),
           ),
         ),
